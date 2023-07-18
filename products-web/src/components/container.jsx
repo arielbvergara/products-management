361fbd803090ee92cbc2e437fc53624f1123c0f3
arielvergarautn <avergara@intercept.cloud>
2023-07-18 23:36:01 +0200

    modifying edit page

--- products-web/src/components/container.jsx
+++ products-web/src/components/container.jsx
@@ -1,6 +1,6 @@
 export default function Container({children}) {
   return (
-    <div className='p-12 rounded-lg border-slate-200 border-solid border-2 border-indigo-600'>
+    <div className='p-12 rounded-[18px] border-slate-200 border-solid border-2 border-indigo-600'>
         {children}
     </div>
   )
